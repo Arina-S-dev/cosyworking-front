f5ca7f4e86a691a45c42d0c23dc7de649d9ad7ec
clement-duport <clement.duports@gmail.com>
2022-09-26 11:44:28 +0200

    base du projet (redux)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  logged: false,
+};
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
