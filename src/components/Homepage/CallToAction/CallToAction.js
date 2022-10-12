4c562d23b9b75153fb510adb46c13fc50caccd38
NicolasWache <nicolaswache@proton.me>
2022-10-12 15:58:54 +0200

    Fix CTO Link

--- src/components/Homepage/CallToAction/CallToAction.js
+++ src/components/Homepage/CallToAction/CallToAction.js
@@ -1,11 +1,12 @@
 import './CallToAction.scss';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function CallToAction() {
   return (
     <div className="CTO">
       <p>Alors, qu'attendez vous pour reserver votre prochain espace de travail ? </p>
-      <Button variant="contained">Effectuer une recherche</Button>
+      <Link to="/recherche"> <Button variant="contained">Effectuer une recherche</Button> </Link>
     </div>
   );
 }
