626b05984ea25ef6b6d5fa124f79d4126778f713
NicolasWache <nicolaswache@proton.me>
2022-10-12 12:08:36 +0200

    Fix CSS

--- src/components/Nav/AccountMenu/AccountMenu.js
+++ src/components/Nav/AccountMenu/AccountMenu.js
@@ -41,9 +41,9 @@ function AccountMenu() {
             onClick={handleClick}
             size="small"
             sx={{
-              ml: 2,
+              ml: 0.5,
               marginTop: '0.3rem',
-              marginRight: '2rem',
+              marginRight: '0.5rem',
             }}
             aria-controls={open ? 'account-menu' : undefined}
             aria-haspopup="true"
@@ -68,17 +68,13 @@ function AccountMenu() {
             '& .MuiAvatar-root': {
               width: 32,
               height: 32,
-              ml: -0.5,
+              ml: 0.5,
               mr: 1,
             },
             '&:before': {
               content: '""',
               display: 'block',
               position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
               bgcolor: 'background.paper',
               transform: 'translateY(-50%) rotate(45deg)',
               zIndex: 0,
@@ -89,7 +85,9 @@ function AccountMenu() {
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         <MenuItem>
-          <Link to="/espace-perso"><Avatar /> Mon Espace Perso</Link>
+          <Link to="/espace-perso">
+            <div className="avatarAccountdiv"><Avatar /> Mon Espace Perso</div>
+          </Link>
         </MenuItem>
         <Divider />
         <MenuItem onClick={getLogout}>
