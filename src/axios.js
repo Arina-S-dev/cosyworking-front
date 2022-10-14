da428c17840bb81742f3700a16eac996857c9b27
Cyrillus <chenetcyril@free.fr>
2022-10-14 16:33:43 +0200

    thunder
    Co-authored-by: clement-duport <clement-duport@users.noreply.github.com>

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const axiosBaseUrl = axios.create({ baseURL: 'http://quentinroggy-server.eddi.cloud/' });
+const axiosBaseUrl = axios.create({ baseURL: 'http://quentinroggy-server.eddi.cloud' });
 
 export default axiosBaseUrl;
